61a9fb5d93859457a2ba68f05912fff745db8730
cindoushka <cindie-baj@live.fr>
2022-12-14 09:32:13 +0100

    diapo done and added at index

--- diaporama.js
+++ diaporama.js
@@ -6,9 +6,9 @@ let timer, elements, slides;
 window.onload = () => {
     //On récupère le diapo
 
-    const diapo = document.querySelector('.diapo');
+    const diapo = document.querySelector(".diapo");
 
-    elements =  document.querySelector('.elements');
+    elements =  document.querySelector(".elements");
 
     //On clone la première image
 
@@ -20,29 +20,57 @@ window.onload = () => {
 
     slides = Array.from(elements.children);
 
+    // let next = document.querySelector("#next");
+
+    // next.addEventListener("click", slideNext);
+
     slideWidth = diapo.getBoundingClientRect().width;
 
-    let repere = document.querySelector('.swipebullet')
+    timer = setInterval(slideNext, 5000);
+
+    repere = document.querySelector('#siwprepere');
+
+    reperes = Array.from(repere.children);
+
+    reperes[0].classList.add("swipebullet-active")
+
 }
 
 // Fonction pour le défilement des diapos
 
-function SlideNext() {
+function slideNext() {
     // Incrémentation du compteur 
-    compteur++;
-    elements.style.transition = "1s linear";
+        compteur++;
+        elements.style.transition = "1s linear";
 
     let decalage = -slideWidth * compteur;
-    elements.style.transform = `"translateX(${decalage}px)"`
 
+    elements.style.transform = `translateX(${decalage}px)`
+    
     // On attend la fin de la transition et on rembobine automatiquement
 
     setTimeout(function() {
         if (compteur >= slides.length  - 1){
             compteur = 0;
             elements.style.transition = "unset";
-            elements.style.transform = "translateX(0)"
+            elements.style.transform = "translateX(0)" 
+         if (compteur == 0) {
+        reperes[0].classList.add("swipebullet-active")
+
+    }
         }
     }, 1000)
+
+
+    for (items of reperes) {
+        items.classList.remove("swipebullet-active")
+    }
+
+  
+
+    if (compteur < slides.length - 1) {
+    reperes[compteur].classList.add("swipebullet-active")
+    }
 }
+
 
