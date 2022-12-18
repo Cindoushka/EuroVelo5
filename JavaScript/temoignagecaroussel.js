0a357167019eacd8f727132bd589d2f967f1b20e
cindoushka <cindie-baj@live.fr>
2022-12-18 20:00:55 +0100

    add arromodif on temoignage js pages

--- JavaScript/temoignagecaroussel.js
+++ JavaScript/temoignagecaroussel.js
@@ -1,11 +1,13 @@
-const urlBdd = "http://62.4.21.200:1337/api/testimonies?populate=*";
-const urlImg = "http://62.4.21.200:1337";
+const urlBddTest = "http://62.4.21.200:1337/api/testimonies?populate=*";
+const urlImgTest = "http://62.4.21.200:1337";
 
 
-let container = document.querySelector('#carousseltemoignage');
+let containerCar = document.querySelector('#carousseltemoignage');
 
+let billets = document.querySelector('#billetstemoignage');
+let note = "/ 5";
 
-fetch(urlBdd)
+fetch(urlBddTest)
     .then(response => response.json())
     .then(data =>{
 
@@ -13,7 +15,7 @@ fetch(urlBdd)
             let article = document.createElement("div")
             let generateHtml=`
                 <a href="/ambassadeurs.html" class="liendudiapotem">
-                    <img src="${urlImg}${data.data[i].attributes.Picture.data[0].attributes.formats.medium.url}" class="imgducarousselt">
+                    <img src="${urlImgTest}${data.data[i].attributes.Picture.data[0].attributes.formats.medium.url}" class="imgducarousselt">
 
                     <div id="textepourimagetem">
                         <img src="Images/coeur.png" alt="logo coeur" class="coeurcaroussel">
@@ -24,7 +26,98 @@ fetch(urlBdd)
                 console.log(data.data[i].attributes.Picture.data[0].attributes.formats.medium.url)
 
                 article.innerHTML = generateHtml
-                container.append(article) 
+                containerCar.append(article) 
              }
 
+             for(let i=0; i< 3; i++) {
+                let article2 = document.createElement("div")
+                let generateHtml=`
+                <article class="billettemoignages">
+                    <p class="publishdate"> ${data.data[i].attributes.DatePublish} </p>
+                    <h2 class="namewitness"> ${data.data[i].attributes.NameWitness} </h2>
+                    <span class="etiquettemap"> ${data.data[i].attributes.description_maps.data[0].attributes.depart} / ${data.data[i].attributes.description_maps.data[0].attributes.arrive} </span>
+    
+    
+                    <div id="notes">
+                        <div class="note">
+                            <p class="note1bdd"> ${data.data[i].attributes.NoteSecurity} ${note}</p>
+                            <p class="typenote">Sécurité</p> 
+                        </div>
+    
+                        <div class="note">
+                            <p class="note1bdd"> ${data.data[i].attributes.NoteBalisage} ${note}</p>
+                            <p class="typenote">Balisage</p> 
+                        </div>
+    
+                        <div class="note">
+                            <p class="note1bdd"> ${data.data[i].attributes.NoteInterest} ${note}</p>
+                            <p class="typenote">Intérêt</p> 
+                        </div>
+    
+                        <div class="note">
+                            <p class="note1bdd"> ${data.data[i].attributes.NoteService} ${note}</p>
+                            <p class="typenote">Services</p> 
+                        </div>
+                    </div> 
+    
+                    <h3 class="titredutemoignage"> ${data.data[i].attributes.Titre} </h3>
+                    <p class="descTemoignage"> ${data.data[i].attributes.Description} </p>
+    
+                    <div class="bottombillet">
+                        <p class="answers"> 0 réponse </p>
+                        <p class="lire">LIRE</p>
+                    </div>
+    
+            </article>`
+            article2.innerHTML = generateHtml
+            billets.append(article2) 
+            }
+
     })
+
+
+    // Variables
+
+    const caroussel = document.querySelector("#carousseltemoignage");
+
+    let nextButton = document.querySelector(".arrowright");
+
+    let prevButton = document.querySelector(".arrowleft");
+
+
+window.onload = () => {
+    //On récupère le diapo
+
+
+    nextButton.addEventListener("click", slideNextButton);
+
+    prevButton.addEventListener("click", slidePrevButton);
+
+
+}
+
+// Fonction pour le défilement des diapos
+
+function slideNextButton() {
+
+    caroussel.style.transition = "1s linear";
+
+    let decalageCar = 490;
+
+    caroussel.style.transform = `translateX(${-decalageCar}px)`
+    
+}
+
+function slidePrevButton() {
+
+    caroussel.style.transition = "1s linear";
+
+    let decalageCar2 = 0;
+
+    caroussel.style.transform = `translateX(${decalageCar2}px)`
+    
+}
+
+
+
+
