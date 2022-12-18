05df6b6b711ad6a7f6cc3ded3fe943e95b1e2b2b
cindoushka <cindie-baj@live.fr>
2022-12-18 20:01:47 +0100

    modif on js pages for temoignages carroussel

--- JavaScript/scrollingNav.js
+++ JavaScript/scrollingNav.js
@@ -33,37 +33,7 @@ window.addEventListener('scroll', () => {
   }
   lastScroll = currentScroll
 
-  console.log(lastScroll)
+  // console.log(lastScroll)
 })
 
 
-const urlBdd = "http://62.4.21.200:1337/api/testimonies?populate=*";
-const urlImg = "http://62.4.21.200:1337";
-
-
-let container = document.querySelector('#carousseltemoignage');
-
-
-fetch(urlBdd)
-    .then(response => response.json())
-    .then(data =>{
-
-        for(let i=0; i< data.meta.pagination.total ; i++) {
-            let article = document.createElement("div")
-            let generateHtml=`
-                <a href="/ambassadeurs.html" class="liendudiapotem">
-                    <img src="${urlImg}${data.data[i].attributes.Picture.data[0].attributes.formats.medium.url}" class="imgducarousselt">
-
-                    <div id="textepourimagetem">
-                        <img src="Images/coeur.png" alt="logo coeur" class="coeurcaroussel">
-                        <p class="textetemoignagec"> Virée de ${data.data[i].attributes.description_maps.data[0].attributes.depart} à ${data.data[i].attributes.description_maps.data[0].attributes.arrive} pour ${data.data[i].attributes.NameWitness}</p>
-                    </div>
-                </a>`
-
-                console.log(data.data[i].attributes.Picture.data[0].attributes.formats.medium.url)
-
-                article.innerHTML = generateHtml
-                container.append(article) 
-             }
-
-    })
--- JavaScript/temoignages.js
+++ JavaScript/temoignages.js
@@ -1,57 +1,59 @@
-const urlBdd = "http://62.4.21.200:1337/api/testimonies?populate=*";
-const urlImg = "http://62.4.21.200:1337";
+// const urlBdd = "http://62.4.21.200:1337/api/testimonies?populate=*";
+// const urlImg = "http://62.4.21.200:1337";
 
 
-let container = document.querySelector('#billetstemoignage');
-let note = "/ 5";
+// let container = document.querySelector('#billetstemoignage');
+// let note = "/ 5";
 
 
-fetch(urlBdd)
-    .then(response => response.json())
-    .then(data =>{
+// fetch(urlBdd)
+//     .then(response => response.json())
+//     .then(data =>{
 
-        for(let i=0; i< 3; i++) {
-            let article = document.createElement("div")
-            let generateHtml=`
-            <article class="billettemoignages">
-                <p class="publishdate"> ${data.data[i].attributes.DatePublish} </p>
-                <h2 class="namewitness"> ${data.data[i].attributes.NameWitness} </h2>
-                <span class="etiquettemap"> ${data.data[i].attributes.description_maps.data[0].attributes.depart} / ${data.data[i].attributes.description_maps.data[0].attributes.arrive} </span>
+//         for(let i=0; i< 3; i++) {
+//             let article = document.createElement("div")
+//             let generateHtml=`
+//             <article class="billettemoignages">
+//                 <p class="publishdate"> ${data.data[i].attributes.DatePublish} </p>
+//                 <h2 class="namewitness"> ${data.data[i].attributes.NameWitness} </h2>
+//                 <span class="etiquettemap"> ${data.data[i].attributes.description_maps.data[0].attributes.depart} / ${data.data[i].attributes.description_maps.data[0].attributes.arrive} </span>
 
 
-                <div id="notes">
-                    <div class="note">
-                        <p class="note1bdd"> ${data.data[i].attributes.NoteSecurity} ${note}</p>
-                        <p class="typenote">Sécurité</p> 
-                    </div>
+//                 <div id="notes">
+//                     <div class="note">
+//                         <p class="note1bdd"> ${data.data[i].attributes.NoteSecurity} ${note}</p>
+//                         <p class="typenote">Sécurité</p> 
+//                     </div>
 
-                    <div class="note">
-                        <p class="note1bdd"> ${data.data[i].attributes.NoteBalisage} ${note}</p>
-                        <p class="typenote">Balisage</p> 
-                    </div>
+//                     <div class="note">
+//                         <p class="note1bdd"> ${data.data[i].attributes.NoteBalisage} ${note}</p>
+//                         <p class="typenote">Balisage</p> 
+//                     </div>
 
-                    <div class="note">
-                        <p class="note1bdd"> ${data.data[i].attributes.NoteInterest} ${note}</p>
-                        <p class="typenote">Intérêt</p> 
-                    </div>
+//                     <div class="note">
+//                         <p class="note1bdd"> ${data.data[i].attributes.NoteInterest} ${note}</p>
+//                         <p class="typenote">Intérêt</p> 
+//                     </div>
 
-                    <div class="note">
-                        <p class="note1bdd"> ${data.data[i].attributes.NoteService} ${note}</p>
-                        <p class="typenote">Services</p> 
-                    </div>
-                </div> 
+//                     <div class="note">
+//                         <p class="note1bdd"> ${data.data[i].attributes.NoteService} ${note}</p>
+//                         <p class="typenote">Services</p> 
+//                     </div>
+//                 </div> 
 
-                <h3 class="titredutemoignage"> ${data.data[i].attributes.Titre} </h3>
-                <p class="descTemoignage"> ${data.data[i].attributes.Description} </p>
+//                 <h3 class="titredutemoignage"> ${data.data[i].attributes.Titre} </h3>
+//                 <p class="descTemoignage"> ${data.data[i].attributes.Description} </p>
 
-                <div class="bottombillet">
-                    <p class="answers"> 0 réponse </p>
-                    <p class="lire">LIRE</p>
-                </div>
+//                 <div class="bottombillet">
+//                     <p class="answers"> 0 réponse </p>
+//                     <p class="lire">LIRE</p>
+//                 </div>
 
-        </article>`
-        article.innerHTML = generateHtml
-        container.append(article) 
-        }
+//         </article>`
+//         article.innerHTML = generateHtml
+//         container.append(article) 
+//         }
 
-    })
+//     })
+
+    
