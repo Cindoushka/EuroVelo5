61a6068f29b8d1f05f9650e14aaaeff30d00cf16
Soaen <sylvain.vansteenkiste59@gmail.com>
2022-12-19 12:23:16 +0100

    Set map link with articles

--- JavaScript/itineraire.js
+++ JavaScript/itineraire.js
@@ -1,19 +1,56 @@
 
 $(document).ready(function () {
-    $(".gps").click(function () {
-            $(".gps").toggleClass("actif");
-            $(".gps").toggleClass("inactif");
-            $(".etapes").toggleClass("actif");
-            $(".etapes").toggleClass("inactif");
+    $(".gps.inactif").click(function () {
+      $(".etapes").removeClass("actif");
+      $(".etapes").addClass("inactif");
+      $(".gps").removeClass("inactif");
+      $(".gps").addClass("actif");
     });
   });
 
   $(document).ready(function () {
     $(".etapes").click(function () {
-        $(".gps").toggleClass("actif");
-        $(".gps").toggleClass("inactif");
-        $(".etapes").toggleClass("actif");
-        $(".etapes").toggleClass("inactif");
+      $(".gps").removeClass("actif");
+      $(".gps").addClass("inactif");
+      $(".etapes").removeClass("inactif");
+      $(".etapes").addClass("actif");
     });
   });
 
+
+
+
+
+
+
+  
+  function descSwitcher(){
+    $(document).ready(function () {
+      $(".description.inactif").click(function () {
+        $(".avis").removeClass("actif");
+        $(".avis").addClass("inactif");
+        $(".description").removeClass("inactif");
+        $(".description").addClass("actif");
+        $(".itineraire-map-avis-div-global").addClass("hidden");
+        $(".itineraire-map-avis-div-global").removeClass("actif");
+        $(".itineraire-map-informations-second-body").removeClass("hidden");
+        $(".itineraire-map-informations-second-body").addClass("actif");
+      });
+    });
+  
+}
+
+  function avisSwitcher(){
+    $(document).ready(function () {
+      $(".avis").click(function () {
+        $(".description").removeClass("actif");
+        $(".description").addClass("inactif");
+        $(".avis").removeClass("inactif");
+        $(".avis").addClass("actif");
+        $(".itineraire-map-avis-div-global").addClass("actif");
+        $(".itineraire-map-avis-div-global").removeClass("hidden");
+        $(".itineraire-map-informations-second-body").addClass("hidden");
+        $(".itineraire-map-informations-second-body").removeClass("actif");
+      });
+    });
+  }
